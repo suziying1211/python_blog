9dab198074f24fc66881bc594ea7dc3856086c6d
ziying <ziying@ziyingdeMacBook-Pro.local>
2022-02-01 22:50:52 +0800

    test git

--- grace.py
+++ grace.py
@@ -1,1 +1,15 @@
-print("test")
+import numpy
+import pandas
+d={'Tom' : 1, 'Mary' : 2, 'Mark' : '3'}
+r= d['Tom'] + d['Mark']
+df=pd.DataFrame([d])
+
+# test_list=[1,5,2,5,2,1]
+# max=test_list[0]
+# indexofmax=0
+
+# for i in range(1,len(test_list)):
+#     if test_list[i] >= max:
+#         max=test_list[i]
+#         indexofmax=i
+# print(indexofmax)
